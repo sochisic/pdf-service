dee083a88fa269667e1d7c2f8ac8f1c45282834b
Stanislav Dagaev <460sochi@gmail.com>
2018-07-24 18:12:36 +0300

    Move fonts to src dir

--- src/routes/pdf/post.js
+++ src/routes/pdf/post.js
@@ -159,10 +159,10 @@ const dateFormat = 'DD/MM/YYYY';
 function createPdfBinary(pdfDoc, callback) {
   const fontDescriptors = {
     Roboto: {
-      normal: path.join(__dirname, '../../../fonts/Roboto-Regular.ttf'),
-      bold: path.join(__dirname, '../../../fonts/Roboto-Medium.ttf'),
-      italics: path.join(__dirname, '../../../fonts/Roboto-Italic.ttf'),
-      bolditalics: path.join(__dirname, '../../../fonts/Roboto-MediumItalic.ttf'),
+      normal: path.join(__dirname, '../../fonts/Roboto-Regular.ttf'),
+      bold: path.join(__dirname, '../../fonts/Roboto-Medium.ttf'),
+      italics: path.join(__dirname, '../../fonts/Roboto-Italic.ttf'),
+      bolditalics: path.join(__dirname, '../../fonts/Roboto-MediumItalic.ttf'),
     },
   };
 
